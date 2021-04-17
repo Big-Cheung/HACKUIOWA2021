cc2fca40e00ce6e17029b65cd4a2c874a6688fbf
Evan Cheung <evan.d.cheung@gmail.com>
2021-04-17 15:48:45 -0700

    consistent spawning

--- GameState.gd
+++ GameState.gd
@@ -8,16 +8,16 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 var meteor = load("res://Meteor.tscn")
+var delay = 1000
 
 func _ready():
 	rand_seed(OS.get_unix_time())
-	for i in range(2):
-		var newMeteor = meteor.instance()
-		add_child(newMeteor)
 
 
 
 func _process(delta):
-	if rand_range(1,50) == 1:
+	if delay >= 1:
 		var newMeteor = meteor.instance()
 		add_child(newMeteor)
+		delay = 0
+	delay += delta
