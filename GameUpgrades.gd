b27385a26191673a0724a20dd3000a8d43a97098
cwphung <81144111+cwphung@users.noreply.github.com>
2021-04-17 22:57:28 -0700

    Purchase Health Added

--- GameUpgrades.gd
+++ GameUpgrades.gd
@@ -3,6 +3,8 @@ extends Node2D
 var newFuelLevel
 var newSpeedLevel
 var newHealthLevel
+var newHealth
+var newHealthPurchased
 var newGrabLevel
 var newRockBalance
 
@@ -16,6 +18,7 @@ func _ready():
 	$"Menu/ShipSpeed/UpgradeSS".connect("pressed", self, "upgradeSpeed")
 	$"Menu/ShipDurability/UpgradeSD".connect("pressed", self, "upgradeHealth")
 	$"Menu/GrabSpeed/UpgradeGS".connect("pressed", self, "upgradeGrab")
+	$"Menu/ExtraHealth/PurchaseHealth".connect("pressed", self, "getHealth")
 	
 	$"Menu/Data/Cancel-button".connect("pressed", self, "setDefaults")
 	$"Menu/Data/Save-close".connect("pressed", self, "saveChanges")
@@ -24,6 +27,8 @@ func setDefaults():
 	newFuelLevel = Globals.fuelLevel
 	newSpeedLevel = Globals.speedLevel
 	newHealthLevel = Globals.healthLevel
+	newHealth = Globals.currentHealth
+	newHealthPurchased = Globals.healthPurchased
 	newGrabLevel = Globals.grabLevel
 	newRockBalance = Globals.rocks
 	updateDisplay()
@@ -43,8 +48,9 @@ func saveChanges():
 	Globals.speedLevel = newSpeedLevel
 	
 	Globals.maxHealth += (int(newHealthLevel) - int(Globals.healthLevel))
-	Globals.currentHealth = Globals.maxHealth
 	Globals.healthLevel = newHealthLevel
+	Globals.currentHealth = newHealth
+	Globals.healthPurchased = newHealthPurchased
 	
 	Globals.grabSpeed += (int(newGrabLevel) - int(Globals.grabLevel)) * 0.2
 	Globals.grabLevel = newGrabLevel
@@ -60,11 +66,13 @@ func updateDisplay():
 	$"Menu/ShipSpeed/FullBarSS/CurrentBarSS".anchor_right = newSpeedLevel/10.0
 	$"Menu/ShipDurability/FullBarSD/CurrentBarSD".anchor_right = newHealthLevel/5.0
 	$"Menu/GrabSpeed/FullBarGS/CurrentBarGS".anchor_right = newGrabLevel/5.0
+	$"Menu/ExtraHealth/CurrentHealthValue".text = str(newHealth)
 	
 	$"Menu/FuelCapacity/UpgradeCost".text = str(Globals.fuelLevelCosts[newFuelLevel])
 	$"Menu/ShipSpeed/UpgradeCost".text = str(Globals.speedLevelCosts[newSpeedLevel])
 	$"Menu/ShipDurability/UpgradeCost".text = str(Globals.healthLevelCosts[newHealthLevel])
 	$"Menu/GrabSpeed/UpgradeCost".text = str(Globals.grabLevelCosts[newGrabLevel])
+	$"Menu/ExtraHealth/UpgradeCost".text = str(100 + (20*newHealthPurchased))
 	$"Menu/Data/points-value".text = str(newRockBalance)
 
 func upgradeFuel():
@@ -86,6 +94,7 @@ func upgradeHealth():
 	if (int(newRockBalance) >= int(cost)) && (int(newHealthLevel) < 5):
 		newRockBalance -= cost
 		newHealthLevel += 1
+		newHealth = 5 + newHealthLevel
 		updateDisplay()	
 		
 func upgradeGrab():
@@ -93,4 +102,12 @@ func upgradeGrab():
 	if (int(newRockBalance) >= int(cost)) && (int(newGrabLevel) < 5):
 		newRockBalance -= cost
 		newGrabLevel += 1
+		updateDisplay()	
+		
+func getHealth():
+	var cost = 100 + (20*newHealthPurchased)
+	if (int(newRockBalance) >= int(cost)) && (int(newHealth) < newHealthLevel + 5):
+		newRockBalance -= cost
+		newHealth += 1
+		newHealthPurchased += 1
 		updateDisplay()	
